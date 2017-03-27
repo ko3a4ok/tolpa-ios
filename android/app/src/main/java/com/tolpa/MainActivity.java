8c61a3fdebaf0ab4c49c79b9336c50ef7d083c9e
roma <ko3a4ok.ua@gmail.com>
2017-03-27 01:15:05 -0700

    [FIX] Permissions for Android Camera and External storage

--- android/app/src/main/java/com/tolpa/MainActivity.java
+++ android/app/src/main/java/com/tolpa/MainActivity.java
@@ -3,8 +3,12 @@ package com.tolpa;
 import android.content.Intent;
 import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity {
+import com.imagepicker.permissions.OnImagePickerPermissionsCallback;
+import com.facebook.react.modules.core.PermissionListener;
 
+public class MainActivity extends ReactActivity implements OnImagePickerPermissionsCallback {
+
+    private PermissionListener listener;
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
@@ -18,5 +22,18 @@ public class MainActivity extends ReactActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void setPermissionListener(PermissionListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (listener != null) {
+          listener.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
